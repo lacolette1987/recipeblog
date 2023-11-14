cc1eebb3f702a8ea84c1c05589c887c31aa87fdb
Colette Güntensperger <c.guentensperger@hotmail.com>
2023-11-14 22:57:00 +0100

    provider

--- src/app/store/StoreProvider.tsx
+++ src/app/store/StoreProvider.tsx
@@ -1,5 +1,5 @@
 import React, {PropsWithChildren} from "react";
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 import {appStore} from "./store";
 
 export const StoreProvider: React.FC<PropsWithChildren> = ({children}) => {
