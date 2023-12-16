8ab28ffec7e9d163417e58f591f9f057aa9624d5
colette8853 <cg@admotion.ch>
2023-12-16 22:33:38 +0100

    auslagern (latestblog, commentsection)

--- src/app/components/commentsection.tsx
+++ src/app/components/commentsection.tsx
@@ -1,40 +1,106 @@
-// import { Card, CardContent, Grid, Typography, Rating } from '@mui/material'
-// import React from 'react'
-// import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
+import React, { useEffect } from 'react';
+import { Typography, Card, CardContent, Grid, Rating } from '@mui/material';
+import AddCommentForm from './add-comment-form';
+import BlankSlateComment from './blankslate/blankslate-comment';
+import useComments from '../hooks/useComments';
+import { FieldValue, Timestamp } from '@firebase/firestore';
+import { useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
+import { RootState } from '../store/store';
+import useBlogs from '../hooks/useBlogs';
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 
 
 
 
-// interface CommentSectionProps {
-//   blogId: string,
-//   title: string;
-// }
+const CommentSection = () => {
 
-// const CommentSection:React.FC<CommentSectionProps> = ({blogId, comments, deleteComment, formatTimestamp}) => {
+    const { blogId } = useParams();
+    const navigate = useNavigate();
+    const currentUser = useSelector((state: RootState) => state.auth.currentUser);
+    const { comments, queryComments, createComment, deleteComment } = useComments();
+    const { blogs, querySingleBlog, deleteBlog } = useBlogs();
 
-  
-//   return (
-//     <Card key={comment.uid} elevation={0} sx={{ marginTop: '30px' }}>
-//         <CardContent>
-//         <Grid container justifyContent={'space-between'}>
-//             <Grid item>
-//             <Typography variant='h4'>{comment.nickname}</Typography>
-//             </Grid>
-//             <Grid item>
-//             <Rating size='small' readOnly value={comment.rating} />
-//             </Grid>
-//         </Grid>
-//         <Typography variant='subtitle1'>
-//             {blogs[0]?.timestamp
-//             ? formatTimestamp(blogs[0]?.timestamp)
-//             : ''}
-//         </Typography>
-//         <Typography>{comment.comment}</Typography>
-//         {currentUser?.uid === comment.authorId ? <DeleteOutlinedIcon onClick={() => deleteComment(blogId!, comment.uid!)} />: ''}
-//         </CardContent>
-//     </Card>
+    
+    const formatTimestamp = (timestamp: FieldValue | Timestamp) => {
+        if (timestamp && timestamp instanceof Timestamp) {
+          return timestamp.toDate().toLocaleString();
+        }
+        return '';
+      };
 
-//   )
-// }
+      
+      useEffect(() => {
+        if (blogId) {
+          queryComments(blogId);
+        }
+      }, [blogId]);
+    
 
-// export default CommentSection
+
+      const handleDelete = async () => {
+        await deleteBlog(blogId!);
+        navigate('/');
+      };
+      
+    return (
+    <>
+      <Typography variant="h3" sx={{ m: '50px 0px 0px 0px' }}>Kommentare</Typography>
+      {currentUser ? (
+        <AddCommentForm
+          submitForm={(comment) =>
+            createComment(blogId!, { ...comment, authorId: currentUser.uid })
+          }
+        />
+      ) : (
+        ''
+      )}
+      {comments.length === 0 ? (
+        <BlankSlateComment />
+      ) : (
+        comments.map((comment) => (
+          <Card key={comment.uid} elevation={0} sx={{ marginTop: '30px' }}>
+            <CardContent>
+              <Grid container justifyContent={'space-between'}>
+                <Grid item>
+                  <Typography variant="h4">{comment.nickname}</Typography>
+                </Grid>
+                <Grid item>
+                  <Rating size="small" readOnly value={comment.rating} />
+                </Grid>
+              </Grid>
+              <Grid
+                container
+                spacing={2}
+                alignItems={'center'}
+                sx={{ mb: '8px' }}
+              >
+                <Grid item>
+                  <Typography variant="subtitle1">
+                    {blogs[0]?.timestamp
+                      ? formatTimestamp(blogs[0]?.timestamp)
+                      : ''}
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  {currentUser?.uid === comment.authorId ? (
+                    <DeleteOutlinedIcon
+                      color="disabled"
+                      fontSize="small"
+                      onClick={() => deleteComment(blogId!, comment.uid!)}
+                    />
+                  ) : (
+                    ''
+                  )}
+                </Grid>
+              </Grid>
+              <Typography>{comment.comment}</Typography>
+            </CardContent>
+          </Card>
+        ))
+      )}
+    </>
+  );
+};
+
+export default CommentSection;
